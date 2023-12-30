7892881a31e01f0f3627f1531bdcf08bf84153c5
ceeygithub <cynthiaekhomwandolor@gmail.com>
2023-12-30 03:54:28 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// dark theme
+let icon = document.querySelector('.moon')
+
+icon.addEventListener('click', () => {
+    document.body.classList.toggle('darkTheme');
+    if (document.body.classList.contains('darkTheme')) {
+
+        icon.className = "fa-solid fa-sun sun"
+        // icon.style.color = 'white';
+
+    }
+    else {
+        icon.className = "fa-solid fa-moon moon";
+
+    }
+})
+
