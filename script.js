1c34e7cd929d9b372df7ea208c3f29310a0f259d
ceeygithub <cynthiaekhomwandolor@gmail.com>
2023-12-30 05:16:07 +0100

    edited code and fixed some of the bugs

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // dark theme
-let icon = document.querySelector('.moon')
+let icon = document.querySelector('.moon');
+let timerIcon = document.querySelector('.timer-icon');
 
 icon.addEventListener('click', () => {
     document.body.classList.toggle('darkTheme');
     if (document.body.classList.contains('darkTheme')) {
 
         icon.className = "fa-solid fa-sun sun"
-        // icon.style.color = 'white';
+        icon.style.color = 'white';
+        // timerIcon.style.color = 'white'
 
     }
     else {
         icon.className = "fa-solid fa-moon moon";
-
+        icon.style.color = 'rgb(61,119,247)';
     }
 })
 
